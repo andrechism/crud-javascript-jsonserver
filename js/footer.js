f7361f8a03fa4b9740ee201b9d0e2ad0601d24aa
André Aragão <57547336+andrechism@users.noreply.github.com>
2020-10-05 22:15:28 -0300

    removido arquivos footer

--- js/footer.js
+++
@@ -1,9 +0,0 @@
-
-
-async function listarLocalidades() {
-  let response = await fetch('https://projeto-pi-digitalhouse.herokuapp.com/posts');
-  let posts = await response.json();
-  console.log(posts);
-}
-
- listarLocalidades();
